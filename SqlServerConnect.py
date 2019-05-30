df3f16199aa984eee57384b4b4a473eac7ec031e
WangXiaoBo <1228793219@qq.com>
2019-05-30 20:42:59 +0800

    sqlserver

---
+++ SqlServerConnect.py
@@ -0,0 +1,22 @@
+import pymssql
+# pip install pymssql
+ 
+# server    数据库服务器名称或IP
+# user      用户名
+# password  密码
+# database  数据库名称
+ 
+server = "192.168.1.5"
+user = "sa"
+password = "P@sswordForSA"
+database = "ZhongYeLearn"
+conn = pymssql.connect(server, user, password, database)
+cursor = conn.cursor()
+cursor.execute("select * from ActivityMayEighteenth")
+row=cursor.fetchone()
+
+while row:
+    print("TableId=%d, PrizeExpect=%s, Target=%s" % (row[0], row[1], row[2]))
+    row = cursor.fetchone()
+
+cursor.close()
